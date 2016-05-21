e3322cc2c2d38b752824dade2acc8b104b59d01e
LCW <CAUCSE@CAUCSE-6P-176>
2016-05-21 17:04:27 +0900

    filemodel,service,mockservice 생성

---
+++ diff/src/diff/FileService.java
@@ -0,0 +1,19 @@
+package diff;
+
+import java.io.File;
+
+public class FileService {
+	FileModel load(Boolean lr,File file,FileModel model){
+		return model;
+	}
+	/**
+	 * 지정한 위치의 파일에 저장합니다
+	 * 
+	 * @param lr 왼쪽이 false,오른쪽 true
+	 * @param file 파일정보.
+	 * @param model 좌우 내용물 정보.
+	 */
+	void save(Boolean lr,File file,FileModel model){
+		
+	}
+}
---
+++ diff/src/diff/MockFileService.java
@@ -0,0 +1,25 @@
+package diff;
+
+import java.io.File;
+
+public class MockFileService extends FileService{
+	FileModel load(Boolean lr,File file,FileModel model){
+		System.out.println("load called : fileName="+file.getPath()+"; lr="+lr);
+		String[] temp = {"abcde","fg","abc","","aa"};
+		if(lr == false) model.setLeft(temp);
+		else model.setRight(temp);
+		return model;
+	}
+	void save(Boolean lr,File file,FileModel model){
+		System.out.println("Mock File Saved to :"+file.getPath());
+		String[] temp;
+		if(lr)
+			temp=model.getRight();
+		else
+			temp=model.getLeft();
+		for(int i=0;i<temp.length;i++)
+		{
+			System.out.println(temp[i]);
+		}
+	}
+}
